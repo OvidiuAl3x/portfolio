26c9ac2738488328d8b8a11fad2ba4f68303d649
Ovidiu <alexandruovidiu1999@gmail.com>
2023-04-04 11:57:11 +0300

    add switcher color

---
+++ js/style-switcher.js
@@ -0,0 +1,27 @@
+//  ============================= toggle style switcher -===============================
+
+const styleSwitcherToogle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToogle.addEventListener("click", () => {
+  document.querySelector(".style-switcher").classList.toggle("open");
+});
+
+//  hide style switcher on scroll
+
+window.addEventListener("scroll", () => {
+  if (document.querySelector(".style-switcher").classList.contains("open")) {
+    document.querySelector(".style-switcher").classList.remove("open");
+  }
+});
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color) {
+  alternateStyles.forEach((style) => {
+    if (color === style.getAttribute("title")) {
+      style.removeAttribute("disabled");
+    } else {
+      style.setAttribute("disabled", "true");
+    }
+  });
+}
+
+console.log(setActiveStyle("color-1"))
