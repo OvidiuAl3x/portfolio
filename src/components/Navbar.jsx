d2815c050683249a374f5666d3e774523dabdc66
OvidiuAl3x <alexandruovidiu1999@gmail.com>
2023-08-10 16:29:05 +0300

    nav finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleClick = () => {
+    setNav(!nav);
+  };
+
   return (
-    <div className="">
-      <MenuIcon className="w-5" />
+    <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
+      <div className="px-2 flex justify-between items-center w-full h-full">
+        <div className="flex items-center w-full">
+          <h1 className="text-3xl font-bold ml-10 sm:text-4xl">Ovidiu</h1>
+          <ul className="hidden md:flex ml-auto mr-10">
+            <li>Home</li>
+            <li>Skills</li>
+            <li>Works</li>
+            <li>Resume</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="md:hidden" onClick={handleClick}>
+          {!nav ? <MenuIcon className="w-5" /> : <XIcon className="w-5" />}
+        </div>
+      </div>
+
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute bg-zinc-200 w-full px-8 transform animate-opacity md:hidden"
+        }
+      >
+        <li className="border-b-2  border-zinc-300 w-full p-4">
+          <p className="transform animate-transform">Home</p>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full p-4">
+          <p className="transform animate-[transform_1.5s]">Skills</p>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full p-4">
+          <p className="transform animate-[transform_2s]">Works</p>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full p-4">
+          <p className="transform animate-[transform_2.5s]">Resume</p>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full p-4">
+          <p className="transform animate-[transform_3s]">Contact</p>
+        </li>
+      </ul>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      keyframes: {
+        opacity: {
+          "0%": { opacity: "0%" },
+          "100%": { opacity: "100%" },
+        },
+        transform: {
+          "0%": { transform: "translate(-200px,0px)" },
+          "100%": { transform: "translate(0px,0px)" },
+        },
+      },
+      animation: {
+        opacity: "opacity 0.5s ease-in-out",
+        transform: "transform 1s",
+      },
+    },
   },
   plugins: [],
-}
-
+};
