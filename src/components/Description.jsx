8094184c60949f33325c14504f117c303b4021af
OvidiuAl3x <alexandruovidiu1999@gmail.com>
2023-08-16 22:45:50 +0300

    works page finished

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Popup from "reactjs-popup";
 import { ImageSlider } from "./ImgSlider";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Description = ({ props, button, LgUsed, slide }) => {
   return (
@@ -10,16 +11,23 @@ const Description = ({ props, button, LgUsed, slide }) => {
       modal
       closeOnDocumentClick
     >
-      <div className="bg-emerald-500 p-3 rounded-lg animate-opacity m-2 ">
-        <h1 className="font-bold text-lg">Short Description</h1>
-        <div className="text-md leading-6">{props}</div>
+      {(close) => (
+        <div className="bg-emerald-500 p-3 rounded-lg animate-opacity m-2 ">
+          <AiOutlineClose
+            className="text-2xl cursor-pointer ml-auto hover:scale-125 bg-black text-emerald-300 rounded-full"
+            onClick={close}
+          />
 
-        <p>
-          <strong>Languages Used: </strong>
-          {LgUsed}
-        </p>
-        {slide ? <ImageSlider slide={slide} /> : null}
-      </div>
+          <h1 className="font-bold text-lg">Short Description</h1>
+          <div className="text-md leading-6">{props}</div>
+
+          <p>
+            <strong>Languages Used: </strong>
+            {LgUsed}
+          </p>
+          {slide ? <ImageSlider slide={slide} /> : null}
+        </div>
+      )}
     </Popup>
   );
 };
